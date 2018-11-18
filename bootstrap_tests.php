5c8577d7942434e1c4940e765e396d0990189edf
Aaron Klump <sourcecode@intheloftstudios.com>
2018-11-18 13:39:37 -0800

    Fix path to webroot.

--- bootstrap_tests.php
+++ bootstrap_tests.php
@@ -12,7 +12,7 @@ require_once dirname(__FILE__) . '/vendor/autoload.php';
  *
  * @var WEB_ROOT
  */
-define('WEB_ROOT', realpath(__DIR__ . '/../../web'));
+define('WEB_ROOT', realpath(__DIR__ . '/../web'));
 
 if (!is_dir(WEB_ROOT)) {
   throw new \RuntimeException("Invalid web root: " . WEB_ROOT);
