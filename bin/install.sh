697b99934828325fd9f8d760fd439adc80a67882
Aaron Klump <sourcecode@intheloftstudios.com>
2018-11-19 11:55:52 -0800

    Adjust scripts.

--- bin/install.sh
+++ bin/install.sh
@@ -4,7 +4,7 @@
 # Install and remove unnecessary files.
 #
 
-[[ -d .git ]] && rm -r .git/
+[[ -d .git ]] && rm -rf .git/
 [[ -d .web_package ]] && rm -r .web_package/
 [[ -d documentation ]] && rm -r documentation/
 [[ -f .gitignore ]] && rm .gitignore
--- bin/update.sh
+++ bin/update.sh
@@ -12,14 +12,15 @@ while [ -h "$source" ]; do # resolve $source until the file is no longer a symli
 done
 root="$( cd -P "$( dirname "$source" )" && pwd )"
 tempdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'temp')
-base=$PWD
+app=$PWD
 
 
 cd $tempdir
-git clone https://github.com/aklump/drupal-test.git tests && (cd tests && ./bin/install.sh) || echo "Installation error, nothing installed."
-
-rsync -av src/ $PWD/src/
-rsync -av docs/ $PWD/docs/
-cp bootstrap_tests.php $PWD/
-cp README.md $PWD/
-cp LICENSE $PWD/
+git clone https://github.com/aklump/drupal-test.git tests
+cd tests
+rsync -a src/ $app/src/
+rsync -a docs/ $app/docs/
+cp bootstrap_tests.php $app/
+cp README.md $app/
+cp LICENSE $app/
+echo && echo "Updated to the latest version."
