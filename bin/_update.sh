a390650337b8d5a4bed2195c23ef7191ae51527e
Aaron Klump <sourcecode@intheloftstudios.com>
2019-04-07 15:36:19 -0700

    Improve on last update.

---
+++ bin/_update.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+#
+# @file
+# Do the bulk of updating.  This is in a separate file so that changes can be made and they will be incorporated immediately.  Otherwise update.sh would have to be run twice when changes occurred.
+#
+
+# Move new files over.
+update_dir bin
+exact_match_dir docs
+exact_match_dir src/DrupalTest
+cp drupal_test_bootstrap.php $app/
+cp drupal_test.yml $app/
+cp README.md $app/
+cp LICENSE $app/
+
+if [[ ! -f "$app/drupal_test_config.yml" ]]; then
+  cp drupal_test_config.yml
+fi
+
+# Update composer.
+if [[ -f "$app/composer--original.json" ]]; then
+  echo "You must delete composer--original.json to continue" && exit 1
+fi
+cp $app/composer.json $app/composer--original.json
+cp composer.json $app/
+
